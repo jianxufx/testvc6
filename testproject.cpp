a0507e55763ffd493955d969c11277d14d58fe01
jianxufx <terrywang361@gmail.com>
2025-02-04 21:38:08 -0800

    max function

--- testproject.cpp
+++ testproject.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
-
+int max(int a,int b);
 int main(int argc, char* argv[])
 {
 	printf("this is a program for test !\n");
 	return 0;
 }
 
+
+int max(int a,int b)
+{
+	if(a<b){
+	
+		return b;
+	}
+
+	return a;
+
+}
