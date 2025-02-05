06c134eea114f5b88a2e66acda7e1f8a6d2511ed
jianxufx <terrywang361@gmail.com>
2025-02-04 21:04:44 -0800

    init

---
+++ testproject.cpp
@@ -0,0 +1,11 @@
+// testproject.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{
+	printf("this is a program for test !\n");
+	return 0;
+}
+
